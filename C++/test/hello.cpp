9322ecc5abf32e7b815f8d34d7488a40c969a4a3
shivsharanyadav2580 <developershiv2580@gmail.com>
2024-09-17 23:23:16 +0530

    Add files via upload

---
+++ C++/test/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+namespace mychai{
+  
+}
+ int main(){
+  
+    cout << "Hello Shivsharan You start to learn C++";
+    return 2;
+ }
+
+ int Shivsharan(){
+   cout << "Hello Shivsharan You start to learn C++ and i have changed it";
+   return0;
+ }
